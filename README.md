d0028a5f1c2f311aae31d20e2802618dc868863f
Marco Maggi <marco.maggi-ipsu@poste.it>
2019-02-16 07:07:37 +0100

    fixed license notices

--- README.md
+++ README.md
@@ -30,11 +30,11 @@ your option) any later version.
 
 This program  is distributed  in the  hope that it  will be  useful, but
 WITHOUT   ANY   WARRANTY;  without   even   the   implied  warranty   of
-MERCHANTABILITY  or  FITNESS FOR  A  PARTICULAR  PURPOSE.  See  the  GNU
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser
 General Public License for more details.
 
-You should have received a copy  of the GNU General Public License along
-with this program.  If not, see <http://www.gnu.org/licenses/>.
+You should have received a copy of the GNU lesser General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 
 ## Install
--- src/ccnames.h
+++ src/ccnames.h
@@ -5,29 +5,25 @@
 
   Abstract
 
-	This  header file  defines a  set  of macros  that generate,  at
-	expansion  time, identifiers  for functions  and variables  that
-	have "well known" roles in the API of the CCLibraries projects.
+	This header  file defines a set  of macros that generate,  at expansion time,
+	identifiers for functions  and variables that have "well known"  roles in the
+	API of the CCLibraries projects.
 
 	It is to be considered experimental.
 
   Copyright (C) 2019 Marco Maggi <marco.maggi-ipsu@poste.it>
 
-  This is free software; you  can redistribute it and/or modify it under
-  the terms of the GNU Lesser General Public License as published by the
-  Free Software  Foundation; either version  3.0 of the License,  or (at
-  your option) any later version.
+  This is free software; you can redistribute  it and/or modify it under the terms of
+  the GNU Lesser General Public License as published by the Free Software Foundation;
+  either version 3.0 of the License, or (at your option) any later version.
 
-  This library  is distributed in the  hope that it will  be useful, but
-  WITHOUT   ANY  WARRANTY;   without  even   the  implied   warranty  of
-  MERCHANTABILITY  or FITNESS  FOR A  PARTICULAR PURPOSE.   See  the GNU
-  Lesser General Public License for more details.
+  This library  is distributed in the  hope that it  will be useful, but  WITHOUT ANY
+  WARRANTY; without  even the implied  warranty of  MERCHANTABILITY or FITNESS  FOR A
+  PARTICULAR PURPOSE.  See the GNU Lesser General Public License for more details.
 
-  You  should have  received a  copy of  the GNU  Lesser  General Public
-  License along  with this library; if  not, write to  the Free Software
-  Foundation, Inc.,  59 Temple Place,  Suite 330, Boston,  MA 02111-1307
-  USA.
-
+  You should have received a copy of the GNU Lesser General Public License along with
+  this library; if not, write to the Free Software Foundation, Inc., 59 Temple Place,
+  Suite 330, Boston, MA 02111-1307 USA.
 */
 
 #ifndef CCNAMES_H
